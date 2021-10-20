a24c8165992b9323b4b56b84102c471090059770
adibose <shiladitya92bose@gmail.com>
2021-10-21 00:22:18 +0530

    first commit

---
+++ src/test/java/com/miniproject/tests/HomePageTests.java
@@ -0,0 +1,21 @@
+package com.miniproject.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class HomePageTests {
+	
+	@Test
+	public void sampleTest() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.in/");
+
+		driver.quit();
+	}
+
+}
